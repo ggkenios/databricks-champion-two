cd35135d1d2b42639d56bf6301d42a702f2f8525
George Gkenios <ggkenios@hotmail.com>
2023-11-28 01:16:29 +0100

    Update the integration test to use the new staging environment

--- tests/integration/integration_test.py
+++ tests/integration/integration_test.py
@@ -28,3 +28,19 @@ def clickstream_filtered():
   # use imported function
   new_cols = columns_except(df, ['prev_id', 'prev_title'])
   return df.select(*new_cols)
+
+# COMMAND ----------
+
+@dlt.table(comment="Check number of records")
+@dlt.expect_or_fail("valid count", "count = 12480649 or count = 0") # we need to check for 0 because DLT first evaluates with empty dataframe
+def filtered_count_check():
+  cnt = dlt.read("clickstream_filtered").count()
+  return spark.createDataFrame([[cnt]], schema="count long")
+
+# COMMAND ----------
+
+@dlt.table(comment="Check type")
+@dlt.expect_all_or_fail({"valid type": "type in ('link', 'redlink')",
+                         "type is not null": "type is not null"})
+def filtered_type_check():
+  return dlt.read("clickstream_filtered").select("type")
